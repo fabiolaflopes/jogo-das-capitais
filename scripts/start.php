4d48f673c19d5f05ac4d7a91d783f835077eb832
Fabiola Ferreira <fabiola.lopes@futcorp.com.br>
2025-01-22 17:47:01 -0300

    primeiro commit

---
+++ scripts/start.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-6">
+
+
+            <div class="card p-5">
+
+                <div class="row">
+                    <div class="col text-center">
+                        <h3>Jogo das Capitais</h3>
+                        <hr>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</div>
